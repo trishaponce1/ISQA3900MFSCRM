b3383e24097fe3a8c8c27505ec72673400085faf
Trisha Ponce <69210074+trishaponce1@users.noreply.github.com>
2021-10-13 18:32:04 -0700

    Update settings.py

--- MaverickFoodServices/settings.py
+++ MaverickFoodServices/settings.py
@@ -117,14 +117,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT =''
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = 'crm/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT =''
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
